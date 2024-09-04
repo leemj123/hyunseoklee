503f16f2a9acd3a93352024eb63ee95435f7e59f
leemj123 <love12mj@naver.com>
2024-09-04 11:12:47 +0900

    init

---
+++ src/main/java/com/aders/employees/hyunseoklee/HyunseokleeApplication.java
@@ -0,0 +1,13 @@
+package com.aders.employees.hyunseoklee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunseokleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyunseokleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aders/employees/hyunseoklee/HyunseokleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aders.employees.hyunseoklee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunseokleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
